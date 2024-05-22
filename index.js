dd91893fd2bd2bd7268660cc038e659edd8dee9b
Mohib <mohibullahmohim2020@gmail.com>
2024-05-23 02:30:19 +0600

    data geting ok

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 const express = require('express') ;
 const cors = require('cors') ;
 const app = express() ;
-require('dotenv').config ;
+require('dotenv').config();
 const port = process.env.PORT || 5000 ;
 
 // middleware
 app.use(cors());
 app.use(express.json()) ;
+
+// mongo db connection
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylmjbhk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+        // Get the database and collection
+        const database = client.db("bistoDB");
+        const menuColl = database.collection("menuCollection");
+        const reviewCollection = database.collection("reviewCollection");
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuColl.find().toArray(); 
+            res.send(result) ;
+        })
+        app.get('/review', async (req, res) => {
+            const cursor = reviewCollection.find() ;
+            const result = await cursor.toArray() ;
+            res.send(result)
+        })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('bisto boss is comming')
